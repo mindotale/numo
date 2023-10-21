80466b74911bb2ac8a2cc29c1514e76a29f35bec
Anna Hordiienko <annagordienko7@gmail.com>
2023-10-21 15:09:24 +0200

    js

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -1,1 +1,12 @@
-﻿
+﻿$(".clear").click(function(){
+
+  $('select option').prop('selected', false);
+});
+
+$.ajax({
+  type: 'GET',
+  url: ' /groups/{123}',
+  success: function(result){
+      console.log(result);
+  }
+});
